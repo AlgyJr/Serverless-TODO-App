3b105ead022b0194de3a9da2234c66a22fa32431
AlgyJr <algymussa@gmail.com>
2022-11-27 18:42:46 +0200

    feat: add generate upload url operation

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -4,16 +4,48 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+import { createAttachmentPresignedUrl } from '../../helpers/attachmentUtils'
+// import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+import { getTodo, updateTodo } from '../../dataLayer/todosAcess'
+
+const logger = createLogger('DeleteTodo')
+const bucket = process.env.ATTACHMENT_S3_BUCKET
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('UploadUrl: ', event)
     const todoId = event.pathParameters.todoId
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+    const todo = await getTodo(todoId)
 
-    return undefined
+    if (todo == null) { // Todo Exists
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: 'Todo does not exist'
+        })
+      }
+    }
+
+    todo.attachmentUrl = `https://${bucket}.s3.amazonaws.com/${todoId}`
+    try {
+      await updateTodo(todo)
+      const url = createAttachmentPresignedUrl(todoId)
+      logger.info('Successfully created signed url.');
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          uploadUrl: url
+        })
+      }
+    } catch (error) {
+      logger.error(`Error ${error.message}`)
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error })
+      }
+    }
   }
 )
 
