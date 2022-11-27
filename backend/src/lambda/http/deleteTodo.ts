cee903837eb9a3728a5f88c174ec3a7f4033fc8e
AlgyJr <algymussa@gmail.com>
2022-11-27 18:42:05 +0200

    feat: add delete todo operation

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -4,15 +4,45 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+import { deleteTodo, getTodo } from '../../dataLayer/todosAcess'
+// import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('DeleteTodo')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
+    logger.info('Processing DeleteTodo event', event)
     // TODO: Remove a TODO item by id
-    
-    return undefined
+    try {
+      const todo = await getTodo(todoId)
+
+      if (todo == null) { // Todo Exists
+        return {
+          statusCode: 404,
+          body: JSON.stringify({
+            error: 'Todo does not exist'
+          })
+        }
+      }
+
+      const result = await deleteTodo(todo);
+      logger.info(`Successfuly deleted todo item: ${todoId}`)
+      
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          result
+        })
+      }
+    } catch (error) {
+      logger.error(`Error ${error.message}`)
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error })
+      }
+    }
   }
 )
 
