b9294e15682da967bd6ce4a1bbc702d65f96b5e0
AlgyJr <algymussa@gmail.com>
2022-11-27 18:41:31 +0200

    feat: add get todos operation

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -3,18 +3,34 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+import { getAllTodos } from '../../dataLayer/todosAcess'
 import { getUserId } from '../utils';
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('GetTodos')
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
+    logger.info('GetTodo Event: ', event)
+    const userId = getUserId(event)
 
-    return undefined
-
+    try {
+      const todos = await getAllTodos(userId)
+      logger.info(`Successfully retrieved todo list`)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({items: todos})
+      }
+    } catch (error) {
+      logger.error(`Error ${error.message}`)
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error })
+      }
+    }
+  }
+)
 handler.use(
   cors({
     credentials: true
