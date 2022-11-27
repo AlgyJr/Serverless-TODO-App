bdedcdcdc9d21bd4a08fafbd3e0d0300d6e97409
AlgyJr <algymussa@gmail.com>
2022-11-27 18:41:12 +0200

    feat: add create todo operation

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,15 +3,39 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { createTodoItem } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+import { createTodo } from '../../dataLayer/todosAcess'
+
+const logger = createLogger('CreateTodo')
+
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Processing CreateTodo event', event)
+
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
 
-    return undefined
+    try {
+      const todo = createTodoItem(newTodo, event)
+      await createTodo(todo)
+      logger.info(`Successfuly created new todo item: ${todo.todoId}`)
+
+      return {
+        statusCode: 201,
+        body: JSON.stringify({
+          item: todo
+        })
+      }
+    } catch (error) {
+      logger.error(`Error ${error.message}`)
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error })
+      }
+    }
+  }
 )
 
 handler.use(
