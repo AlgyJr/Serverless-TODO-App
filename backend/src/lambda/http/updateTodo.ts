856750b8450eff222cdfdf7ccc68c801747e4d0f
AlgyJr <algymussa@gmail.com>
2022-11-27 18:41:54 +0200

    feat: add update todo operation

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -4,18 +4,38 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { updateTodo } from '../../businessLogic/todos'
+import { createUpdateTodoItem } from '../../businessLogic/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('UpdateTodo')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Processing UpdateTodo event', event)
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
 
-
-    return undefined
+    const userId = getUserId(event)
+    try {
+      const result = await createUpdateTodoItem(todoId, updatedTodo, userId)
+      logger.info(`Successfuly updated todo item: ${todoId}`)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          result
+        })
+      }
+    } catch (error) {
+      logger.error(`Error ${error.message}`)
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error })
+      }
+    }
+  }
 )
 
 handler
